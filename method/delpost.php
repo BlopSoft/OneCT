44f26dd47b042191a31f238adfdd9d3a38687fd7
KovshKomeij <85364286+KovshKomeij@users.noreply.github.com>
2024-01-03 01:26:43 +0400

    Add files via upload

--- method/delpost.php
+++ method/delpost.php
@@ -7,11 +7,11 @@
 	$postdata = mysqli_fetch_assoc($postinf);
 
 	if(token_data($_SESSION['user']['access_token'])['error'] == 0){
-		if(mysqli_query($db, $delete)){
-			unlink($postdata['img']);
-			header("Location: " .$_SERVER['HTTP_REFERER']);
-		} else {
-			header("Location: " .$_SERVER['HTTP_REFERER']);
+		if($postdata['id_user'] or $postdata['id_who'] == $_SESSION['user']['user_id']){
+			if(mysqli_query($db, $delete)){
+				unlink($postdata['img']);
+				header("Location: " .$_SERVER['HTTP_REFERER']);
+			}
 		}
 	} else {
 		$error = "Bad request / token";
--- method/pinpost.php
+++ method/pinpost.php
@@ -5,14 +5,17 @@
 	$pin = 'UPDATE post SET pin = 1 WHERE id = "' .(int)$_GET['id']. '"';
 	$unpin = 'UPDATE post SET pin = 0 WHERE id = "' .(int)$_GET['id']. '"';
     $postinf = mysqli_query($db, 'SELECT * FROM post WHERE id = ' .(int)$_GET['id']);
+	$postdata = mysqli_fetch_assoc($postinf);
 
 	if(token_data($_SESSION['user']['access_token'])['error'] == 0){
-		if(mysqli_fetch_assoc($postinf)['pin'] == 1){
-			mysqli_query($db, $unpin);
-			header("Location: " .$_SERVER['HTTP_REFERER']);
-		} elseif (mysqli_fetch_assoc($postinf)['pin'] == 0){
-			mysqli_query($db, $pin);
-			header("Location: " .$_SERVER['HTTP_REFERER']);
+		if($postdata['id_user'] or $postdata['id_who'] == $_SESSION['user']['user_id']){
+			if($postdata['pin'] == 1){
+				mysqli_query($db, $unpin);
+				header("Location: " .$_SERVER['HTTP_REFERER']);
+			} elseif($postdata['pin'] == 0){
+				mysqli_query($db, $pin);
+				header("Location: " .$_SERVER['HTTP_REFERER']);
+			}
 		}
 	} else {
 		$error = "Bad request / token";
