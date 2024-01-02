d0a2e72aa5a53e8566789bc255e952a9d4b53d08
KovshKomeij <85364286+KovshKomeij@users.noreply.github.com>
2024-01-03 01:41:03 +0400

    Add files via upload

--- method/delpost.php
+++ method/delpost.php
@@ -6,8 +6,10 @@
     $postinf = mysqli_query($db, 'SELECT * FROM post WHERE id = ' .(int)$_GET['id']);
 	$postdata = mysqli_fetch_assoc($postinf);
 
+	$user_data = mysqli_fetch_assoc(mysqli_query($db, 'SELECT * FROM users WHERE id = ' .$_SESSION['user']['user_id']));
+
 	if(token_data($_SESSION['user']['access_token'])['error'] == 0){
-		if($postdata['id_user'] or $postdata['id_who'] == $_SESSION['user']['user_id']){
+		if($postdata['id_user'] or $postdata['id_who'] == $_SESSION['user']['user_id'] or $user_data['priv'] >= 2){
 			if(mysqli_query($db, $delete)){
 				unlink($postdata['img']);
 				header("Location: " .$_SERVER['HTTP_REFERER']);
