8a741ae2bff24db7857d61bc3de472cef8577604
KovshKomeij <85364286+KovshKomeij@users.noreply.github.com>
2024-01-03 00:45:57 +0400

    Add files via upload

--- method/delpost.php
+++ method/delpost.php
@@ -1,11 +1,12 @@
 <?php
 	require_once "../include/config.php";
+	include '../include/user.php';
 
 	$delete = 'DELETE FROM post WHERE id = "' .(int)$_GET['id']. '"';
-    $postinf = mysqli_query($db, 'SELECT * FROM post WHERE id = "' .(int)$_GET['id']. '" AND id_user = "' .$_SESSION['user']['user_id']. '"');
+    $postinf = mysqli_query($db, 'SELECT * FROM post WHERE id = ' .(int)$_GET['id']);
 	$postdata = mysqli_fetch_assoc($postinf);
 
-	if(mysqli_num_rows($postinf) == 1){
+	if(token_data($_SESSION['user']['access_token'])['error'] == 0){
 		if(mysqli_query($db, $delete)){
 			unlink($postdata['img']);
 			header("Location: " .$_SERVER['HTTP_REFERER']);
@@ -13,6 +14,8 @@
 			header("Location: " .$_SERVER['HTTP_REFERER']);
 		}
 	} else {
-		header("Location: " .$_SERVER['HTTP_REFERER']);
+		$error = "Bad request / token";
 	}
+
+	echo($error);
 ?>
--- method/pinpost.php
+++ method/pinpost.php
@@ -1,21 +1,22 @@
 <?php
 	require_once "../include/config.php";
+	include '../include/user.php';
 
 	$pin = 'UPDATE post SET pin = 1 WHERE id = "' .(int)$_GET['id']. '"';
 	$unpin = 'UPDATE post SET pin = 0 WHERE id = "' .(int)$_GET['id']. '"';
-    $postinf = mysqli_query($db, 'SELECT * FROM post WHERE id = "' .(int)$_GET['id']. '" AND id_user = "' .$_SESSION['user']['user_id']. '"');
+    $postinf = mysqli_query($db, 'SELECT * FROM post WHERE id = ' .(int)$_GET['id']);
 
-	if(mysqli_num_rows($postinf) == 1){
-
+	if(token_data($_SESSION['user']['access_token'])['error'] == 0){
 		if(mysqli_fetch_assoc($postinf)['pin'] == 1){
 			mysqli_query($db, $unpin);
-		 	header("Location: " .$_SERVER['HTTP_REFERER']);
+			header("Location: " .$_SERVER['HTTP_REFERER']);
 		} elseif (mysqli_fetch_assoc($postinf)['pin'] == 0){
 			mysqli_query($db, $pin);
 			header("Location: " .$_SERVER['HTTP_REFERER']);
 		}
-		
 	} else {
-		$error = "Bad request / Access Denied";
+		$error = "Bad request / token";
 	}
+
+	echo($error);
 ?>
