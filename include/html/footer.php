fc19f5c8a7a6ebdd4cefbf772d54f485f5c8b7a9
KovshKomeij <85364286+KovshKomeij@users.noreply.github.com>
2024-01-03 21:59:13 +0400

    Add files via upload

---
+++ include/html/footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <div class="links">
+        <a href="<?php echo($url); ?>/web/terms.php" class="link">Пользовательское соглашение</a>
+        <a href="<?php echo($url); ?>/web/authors.php" class="link">Авторы</a>
+    </div>
+    <p>php: <?php echo(phpversion()); ?> | MySQL: <?php echo(mysqli_get_server_info($db)); ?></p>
+    <a href="https://github.com/blopsoft/onect">Github</a>
+     | 
+    <a href="https://t.me/blopsoft">Telegram</a>
+</div>
