89dd3b9861d62fea49e67a77e13e80f2df28811f
dibof228 <85364286+dibof228@users.noreply.github.com>
2024-09-20 20:18:13 +0400

    Update delpost.php
    Убрал уязвимость

--- methods/delpost.php
+++ methods/delpost.php
@@ -9,7 +9,7 @@
 	$user_data = mysqli_fetch_assoc(mysqli_query($db, 'SELECT * FROM users WHERE id = ' .$_SESSION['user']['user_id']));
 
 	if(token_data($_SESSION['user']['access_token'])['error'] == 0){
-		if($postdata['id_user'] or $postdata['id_who'] == $_SESSION['user']['user_id'] or $user_data['priv'] >= 2){
+		if($postdata['id_user'] == $_SESSION['user']['user_id'] or $user_data['priv'] >= 2){
 			if(mysqli_query($db, $delete)){
 				mysqli_query($db, 'DELETE FROM likes WHERE post_id = ' .(int)$_GET['id']);
 				mysqli_query($db, 'DELETE FROM comments WHERE post_id = ' .(int)$_GET['id']);
