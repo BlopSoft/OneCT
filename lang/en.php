ef5e52de114913853b23a07b9add99e6b071906c
Anon <174414990+gamma63@users.noreply.github.com>
2024-10-07 21:39:28 +0300

    english

---
+++ lang/en.php
@@ -0,0 +1,69 @@
+<?php 
+    $lang_name = "English";
+
+    // head
+
+    $lang_user_head = "User ";
+    $lang_wall_head = "Wall";
+
+    //Header
+
+    $lang_login = "Login";
+    $lang_register = "Register";
+
+    $lang_logout = "Logout";
+    $lang_search = "Search";
+    $lang_wall = "Wall";
+    $lang_home = "Home";
+
+    // Body
+
+    $lang_welcome = "
+        <p>For using $sitename, you need to register</p>
+    ";
+    $lang_rules = "
+        <h3>$sitename Terms of Use</h3>
+        <p>
+            1. The site prohibits politics, porn, shock content and content banned in your country.
+            2. Advertising on the site is allowed only if it does not advertise content prohibited in your country or porn with shock content.
+            3. Administrators can delete your posts and you can ask why they are deleted.
+            4. Violation of the rules will result in deletion or banning of your account or posts from there.
+            5. Admins can ban your account for any reason and specify the reason for it, and if you are banned without a reason, then write to @dibof228 for what you were banned.<br>
+        </p>
+        <h3>We get data such as: IP address (For anti-spam accounts), Email and all data from OneCT.</h3>
+    ";
+    $lang_authors1 = "Gamma World is supported by blopsoft";
+    $lang_authors2 = "$sitename mods:";
+    $lang_authors3 = "The OneCT is licenced under BSD-3 Clause license";
+
+    $lang_nickname = "Name: ";
+    $lang_yespost = "Allow to write peoples on my wall:";
+    $lang_langnt = "Language:";
+    $lang_change = "Edit";
+    $lang_changepass = "Edit password";
+
+    $lang_find = "Found: ";
+    $lang_find_users = " users";
+    $lang_admin = "Admin Panel";
+    $lang_change_avatar = "Change avatar";
+    $lang_settings = "Settings";
+    $lang_description = "Description: ";
+
+    $lang_yes = "Allowed";
+    $lang_no = "Not allowed";
+    
+    $lang_publish = "Publish";
+    $lang_attach = "Attach";
+    $lang_image = "Picture:";
+    $lang_pinned = "Pinned";
+    $lang_date = "d M Y at H:i";
+    $lang_byname = "From: ";
+    $lang_nowall = "There is no posts on the wall";
+    $lang_previous = "Previous page";
+    $lang_next = "Next page";
+
+    // Footer
+
+    $lang_terms = 'Terms Of Use';
+    $lang_authors = 'Authors';
+?>
